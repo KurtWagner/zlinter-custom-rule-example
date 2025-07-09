4b66835746deaa6725d4e597a5043f6b5227e2dd
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2025-07-09 11:08:31 +1000

    Update to latest master that had breaking changes

--- src/example_rule.zig
+++ src/example_rule.zig
@@ -4,28 +4,28 @@ const std = @import("std");
 const zlinter = @import("zlinter");
 
 pub const Config = struct {
-    severity: zlinter.LintProblemSeverity = .warning,
+    severity: zlinter.rules.LintProblemSeverity = .warning,
 };
 
-pub fn buildRule(options: zlinter.LintRuleOptions) zlinter.LintRule {
+pub fn buildRule(options: zlinter.rules.LintRuleOptions) zlinter.rules.LintRule {
     _ = options;
 
-    return zlinter.LintRule{
+    return zlinter.rules.LintRule{
         .rule_id = "example_rule",
         .run = &run,
     };
 }
 
 fn run(
-    rule: zlinter.LintRule,
-    _: zlinter.LintContext,
-    doc: zlinter.LintDocument,
+    rule: zlinter.rules.LintRule,
+    _: zlinter.session.LintContext,
+    doc: zlinter.session.LintDocument,
     allocator: std.mem.Allocator,
-    options: zlinter.LintOptions,
-) error{OutOfMemory}!?zlinter.LintResult {
+    options: zlinter.session.LintOptions,
+) error{OutOfMemory}!?zlinter.results.LintResult {
     const config = options.getConfig(Config);
 
-    var lint_problems = std.ArrayListUnmanaged(zlinter.LintProblem).empty;
+    var lint_problems = std.ArrayListUnmanaged(zlinter.results.LintProblem).empty;
     defer lint_problems.deinit(allocator);
 
     const tree = doc.handle.tree;
@@ -46,7 +46,7 @@ fn run(
     }
 
     return if (lint_problems.items.len > 0)
-        try zlinter.LintResult.init(
+        try zlinter.results.LintResult.init(
             allocator,
             doc.path,
             try lint_problems.toOwnedSlice(allocator),
