f7d3b77138a7c2b89316121c5114bcf57ddf69b9
Kurt Wagner <1269283+KurtWagner@users.noreply.github.com>
2025-09-22 19:22:32 +1000

    Update latest zlinter

--- src/example_rule.zig
+++ src/example_rule.zig
@@ -18,14 +18,15 @@ pub fn buildRule(options: zlinter.rules.RuleOptions) zlinter.rules.LintRule {
 
 fn run(
     rule: zlinter.rules.LintRule,
-    doc: zlinter.session.LintDocument,
-    allocator: std.mem.Allocator,
+    _: *zlinter.session.LintContext,
+    doc: *const zlinter.session.LintDocument,
+    gpa: std.mem.Allocator,
     options: zlinter.rules.RunOptions,
 ) error{OutOfMemory}!?zlinter.results.LintResult {
     const config = options.getConfig(Config);
 
     var lint_problems = std.ArrayListUnmanaged(zlinter.results.LintProblem).empty;
-    defer lint_problems.deinit(allocator);
+    defer lint_problems.deinit(gpa);
 
     const tree = doc.handle.tree;
     var token: std.zig.Ast.TokenIndex = 0;
@@ -33,12 +34,12 @@ fn run(
         if (tree.tokens.items(.tag)[token] == .string_literal) {
             const name = tree.tokenSlice(token);
             if (std.ascii.indexOfIgnoreCase(name, "Hello") != null) {
-                try lint_problems.append(allocator, .{
+                try lint_problems.append(gpa, .{
                     .rule_id = rule.rule_id,
                     .severity = config.severity,
                     .start = .startOfToken(tree, token),
                     .end = .endOfToken(tree, token),
-                    .message = try allocator.dupe(u8, "No hello!"),
+                    .message = try gpa.dupe(u8, "No hello!"),
                 });
             }
         }
@@ -46,9 +47,9 @@ fn run(
 
     return if (lint_problems.items.len > 0)
         try zlinter.results.LintResult.init(
-            allocator,
+            gpa,
             doc.path,
-            try lint_problems.toOwnedSlice(allocator),
+            try lint_problems.toOwnedSlice(gpa),
         )
     else
         null;
